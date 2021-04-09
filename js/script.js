b6856b82ac1d81228fdef6f416b2738bcb7de42a
Calavera <calavera@localhost.localdomain>
2021-04-09 16:11:17 +0200

    microcambiamenti grafici

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ $(function () {
     $(this).removeClass('show');
   })
 });
+// TODO
+// $(function () {
+//   $('nav .lang i').mouseenter(function () {
+//     add chevron-down class
+//   })
+//   $('nav .lang i').mouseleave(function () {
+//    rm chevron-down class
+//   })
+// });
